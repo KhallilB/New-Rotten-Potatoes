ab904f8d68e565e7664e22ec7245af2193058482
KhallilB <khallil.bailey@gmail.com>
2018-09-30 10:10:21 -0700

    Added Index Route

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//HOME
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World '})
-})
+//MOCK ARRAY OF REVIEWS
+let reviews = [
+    { title: 'One Review' },
+    { title: 'Another Review' }
+]
+
+//INDEX
+app.get('/', (req,res) => {
+    res.render('reviews-index', { reviews: reviews });
+});
+
 
 app.listen(3000, () => {
-    console.log('We Are Working!')
+    console.log('We Are Working!');
 })
