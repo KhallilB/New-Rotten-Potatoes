8d705671003a442e825fe25437fe06478c429987
KhallilB <khallil.bailey@gmail.com>
2018-09-30 09:32:18 -0700

    Added Home Route Using Express

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//Using Express
 const express = require('express');
 const app = express();
 
+//Using Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//HOME
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home', { msg: 'Hello World '})
 })
 
 app.listen(3000, () => {
